e63c48be6848241df6ed99b0a5f8065f56672e48
Michelle Li <rphmichelle@gmail.com>
2023-06-29 10:10:39 -0400

    completing setup

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-const paths = document.querySelectorAll('path');
-const popup = document.getElementById('popup');
+document.addEventListener("DOMContentLoaded", () => {
 
-function createPopup(event) {
-    const countryName = event.target.getAttribute('title');
-    const rect = event.target.getBoundingClientRect();
-    let centerX = rect.left + rect.width / 2;
-    let centerY = rect.top + rect.height / 2;
-    popup.innerText = countryName;
-    //  css?
-    popup.style.display = 'block';
-    popup.style.left = centerX + 'px';
-    popup.style.top = centerY + 'px';
-}
+  const paths = document.querySelectorAll('path');
+  const popup = document.getElementById('popup');
 
-paths.forEach(path => {
-  path.addEventListener('mouseover', createPopup)
+  function createPopup(event) {
+      const countryName = event.target.getAttribute('title');
+      const rect = event.target.getBoundingClientRect();
+      let centerX = rect.left + rect.width / 2;
+      let centerY = rect.top + rect.height / 2;
+      popup.innerText = countryName;
+      popup.style.display = 'block';
+      popup.style.left = centerX + 'px';
+      popup.style.top = centerY + 'px';
+  }
 
-  path.addEventListener('mouseout', function() {
-    popup.style.display = 'none';
-  });
-})
+  paths.forEach(path => {
+    path.addEventListener('mouseover', createPopup)
 
+    path.addEventListener('mouseout', function() {
+      popup.style.display = 'none';
+    });
+  })
 
-
+});
